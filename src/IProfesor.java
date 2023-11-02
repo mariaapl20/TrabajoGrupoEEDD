45f80bfef9b3c5accdf87f049bc3c858c2414e74
Fernando José Romero Palacios <58937175+Fernandorp7@users.noreply.github.com>
2023-11-02 14:20:34 +0100

    Delete src directory

--- src/IProfesor.java
+++
@@ -1,9 +0,0 @@
-
-public interface IProfesor {
-	public String getsUniversidad();
-	public void setsUniversidad(String sUniversidad);
-	public int getiNumAlumnos();
-	public void setiNumAlumnos(int iNumAlumnos);
-	public boolean isbFuncionario();
-	public void setbFuncionario(boolean bFuncionario);
-}
